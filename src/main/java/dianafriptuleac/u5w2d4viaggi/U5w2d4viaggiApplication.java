d42f4688b58c078a90683ab6dd34dd60582f6bf7
Diana <dianadorojuc3@gmail.com>
2024-10-31 15:24:33 +0100

    Initial commit

---
+++ src/main/java/dianafriptuleac/u5w2d4viaggi/U5w2d4viaggiApplication.java
@@ -0,0 +1,13 @@
+package dianafriptuleac.u5w2d4viaggi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5w2d4viaggiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5w2d4viaggiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dianafriptuleac/u5w2d4viaggi/U5w2d4viaggiApplicationTests.java
@@ -0,0 +1,13 @@
+package dianafriptuleac.u5w2d4viaggi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5w2d4viaggiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
